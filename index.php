8848d40bca1197fea68312d18279c8a979461f0b
luke <21106956+logburn@users.noreply.github.com>
2020-12-19 21:04:23 -0500

    added some stuff to index

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
 <?php include "top.php"; ?>
 
   <div id=content>
-    <p>My name is <span class="letter">S</span><span class="letter">a</span><span class="letter">b</span><span class="letter">r</span><span class="letter">i</span><span class="letter">n</span><span class="letter">a</span> <span class="letter">C</span><span class="letter">h</span><span class="letter">i</span><span class="letter">n</span>, and I'm BIT major part of the Calhoun Discovery Program. I am passionate about making education accessible so everyone and believe that technology can bring education to everyone.
+    <p>My name is <span class="letter">S</span><span class="letter">a</span><span class="letter">b</span><span class="letter">r</span><span class="letter">i</span><span class="letter">n</span><span class="letter">a</span> <span class="letter">C</span><span class="letter">h</span><span class="letter">i</span><span class="letter">n</span>, and I'm BIT major part of the Calhoun Discovery Program. I am passionate about making education accessible so everyone and believe that technology can bring education to everyone. I am also the girlfriend of <a href=https://lukeogburn.com>Luke Ogburn</a>, an awesome boyfriend who always loves me. He is so super cool and awesome.
     </p>
+    <p>hai this iss walker and i waanted to say thet i lov tristan becas he is very nise and has nise horns</p>
   </div>
 
   <h2 class=name>CONNECT</h2>
